3c614b325f19db385f99117b29a8e287546e1733
Pieniger <34837513+Pieniger@users.noreply.github.com>
2025-09-16 23:10:12 +0200

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,5 +2,5 @@ plugins {
     id("com.android.application") version "8.5.2" apply false
     id("org.jetbrains.kotlin.android") version "2.0.0" apply false
     id("org.jetbrains.kotlin.plugin.serialization") version "2.0.0" apply false
-    id("org.jetbrains.kotlin.plugin.compose") version "2.0.0" apply false // <-- required for Compose with Kotlin 2.0
+    id("org.jetbrains.kotlin.plugin.compose") version "2.0.0" apply false // required for Compose on Kotlin 2.0
 }
