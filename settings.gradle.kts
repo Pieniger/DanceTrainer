31a379c022b2fb693e98270a4fa6ac2262cd9172
Pieniger <34837513+Pieniger@users.noreply.github.com>
2025-11-11 12:12:48 +0000

    Refactor pluginManagement and dependency repositories

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,9 +1,21 @@
 pluginManagement {
-    repositories { gradlePluginPortal(); google(); mavenCentral() }
+    repositories {
+        gradlePluginPortal()
+        google()
+        mavenCentral()
+    }
+    plugins {
+        id("com.android.application") version "8.6.1"
+        id("org.jetbrains.kotlin.android") version "2.0.20"
+        id("org.jetbrains.kotlin.plugin.compose") version "2.0.20"
+    }
 }
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
-    repositories { google(); mavenCentral() }
+    repositories {
+        google()
+        mavenCentral()
+    }
 }
 rootProject.name = "DanceTrainer"
 include(":app")
