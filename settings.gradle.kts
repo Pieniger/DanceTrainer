ec2985d3d5e55bf03d7c199bd733fba5137347e3
Pieniger <34837513+Pieniger@users.noreply.github.com>
2025-11-13 17:43:11 +0000

    Update plugin management and remove dependency resolution

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,3 +1,4 @@
+// (what you already have)
 pluginManagement {
     repositories {
         gradlePluginPortal()
@@ -10,12 +11,3 @@ pluginManagement {
         id("org.jetbrains.kotlin.plugin.compose") version "2.0.20"
     }
 }
-dependencyResolutionManagement {
-    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
-    repositories {
-        google()
-        mavenCentral()
-    }
-}
-rootProject.name = "DanceTrainer"
-include(":app")
