329eff0c9050a31a24cc28356369f944c4c1b7da
Pieniger <34837513+Pieniger@users.noreply.github.com>
2025-11-13 18:04:09 +0000

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
-    // Versions are defined in settings.gradle.kts (pluginManagement)
     id("com.android.application") apply false
     id("org.jetbrains.kotlin.android") apply false
     id("org.jetbrains.kotlin.plugin.compose") apply false
+    id("org.jetbrains.kotlin.plugin.serialization") apply false
 }
