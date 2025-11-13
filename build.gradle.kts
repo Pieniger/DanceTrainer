70e43f22c9c1c4b6225ca8345b96dd0e7bb247f6
Pieniger <34837513+Pieniger@users.noreply.github.com>
2025-11-13 17:42:30 +0000

    Update build.gradle.kts to remove plugin versions
    Removed version specifications for Android and Kotlin plugins.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
-    id("com.android.application") version "8.5.2" apply false
-    id("org.jetbrains.kotlin.android") version "2.0.0" apply false
-    id("org.jetbrains.kotlin.plugin.compose") version "2.0.0" apply false
-    id("org.jetbrains.kotlin.plugin.serialization") version "2.0.0" apply false
+    // Versions are defined in settings.gradle.kts (pluginManagement)
+    id("com.android.application") apply false
+    id("org.jetbrains.kotlin.android") apply false
+    id("org.jetbrains.kotlin.plugin.compose") apply false
 }
