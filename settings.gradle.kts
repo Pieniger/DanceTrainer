9e35d421f2d4e1aca9f75e309b6548f45af2e6a3
Pieniger <34837513+Pieniger@users.noreply.github.com>
2025-11-13 18:19:35 +0000

    Update settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -8,6 +8,7 @@ pluginManagement {
         id("com.android.application") version "8.6.1"
         id("org.jetbrains.kotlin.android") version "2.0.20"
         id("org.jetbrains.kotlin.plugin.compose") version "2.0.20"
+        id("org.jetbrains.kotlin.plugin.serialization") version "2.0.20"
     }
 }
 
